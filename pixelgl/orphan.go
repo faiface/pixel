08af9e401081a69dbe8edc88a268c1fee56e2f9f
faiface <faiface@ksp.sk>
2016-11-24 14:24:17 +0100

    add orphan function Clear

---
+++ pixelgl/orphan.go
@@ -0,0 +1,13 @@
+package pixelgl
+
+import "github.com/go-gl/gl/v3.3-core/gl"
+
+// This file defines functions that can operate without a parent BeginEnder.
+
+// Clear clears the current context..
+func Clear(r, g, b, a float64) {
+	DoNoBlock(func() {
+		gl.ClearColor(float32(r), float32(g), float32(b), float32(a))
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+	})
+}
