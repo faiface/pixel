c192b684d3685f85ae8376d633f7c17df934d3cf
faiface <faiface@ksp.sk>
2016-12-01 17:49:40 +0100

    fix doc

--- pixelgl/orphan.go
+++ pixelgl/orphan.go
@@ -4,7 +4,7 @@ import "github.com/go-gl/gl/v3.3-core/gl"
 
 // This file defines functions that can operate without a parent Doer.
 
-// Clear clears the current context..
+// Clear clears the current OpenGL context.
 func Clear(r, g, b, a float32) {
 	DoNoBlock(func() {
 		gl.ClearColor(r, g, b, a)
