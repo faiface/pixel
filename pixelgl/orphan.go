73db1b4d5ba46c3cb4d91649f915191297687a2d
faiface <faiface@ksp.sk>
2016-12-01 17:48:52 +0100

    change all float64 to float32 in pixelgl

--- pixelgl/orphan.go
+++ pixelgl/orphan.go
@@ -5,9 +5,9 @@ import "github.com/go-gl/gl/v3.3-core/gl"
 // This file defines functions that can operate without a parent Doer.
 
 // Clear clears the current context..
-func Clear(r, g, b, a float64) {
+func Clear(r, g, b, a float32) {
 	DoNoBlock(func() {
-		gl.ClearColor(float32(r), float32(g), float32(b), float32(a))
+		gl.ClearColor(r, g, b, a)
 		gl.Clear(gl.COLOR_BUFFER_BIT)
 	})
 }
