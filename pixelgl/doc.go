db0d646ca88416989511830b757be01eb9155234
faiface <faiface@ksp.sk>
2016-12-31 02:43:02 +0100

    update doc

--- pixelgl/doc.go
+++ pixelgl/doc.go
@@ -1,5 +1,7 @@
-// Package pixelgl provides higher-level abstractions around the basic OpenGL primitives and
+// Package pixelgl provides some abstractions around the basic OpenGL primitives and
 // operations.
+//
+// All calls should be wrapped inside pixelgl.Do/DoNoBlock/DoErr/DoVal.
 //
 // This package deliberately does not handle nor report OpenGL errors, it's up to you to
 // cause none.
