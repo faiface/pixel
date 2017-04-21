4aea56198f0634319e1a32a532c349d96d1a131f
faiface <faiface@ksp.sk>
2017-04-21 17:00:18 +0200

    add pixelgl package doc

---
+++ pixelgl/doc.go
@@ -0,0 +1,5 @@
+// Package pixelgl implements efficient OpenGL targets and utilities for the Pixel game development
+// library, specifically Window and Canvas.
+//
+// It also contains a few additional utilities to help extend Pixel with OpenGL graphical effects.
+package pixelgl
