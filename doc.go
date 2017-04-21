497df7842b334d1742f5e6e2ae3e9f291b5a5052
faiface <faiface@ksp.sk>
2017-04-21 16:57:08 +0200

    add pixel doc

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Package pixel implements platform and backend agnostic core of the Pixel game development
+// library.
+//
+// It contains the core Target, Triangles, Picture pattern and implements standard elements, such as
+// Sprite, Batch, Vec, Rect and RGBA in addition to the basic Triangles and Picture implementations:
+// TrianglesData and PictureData.
+package pixel
