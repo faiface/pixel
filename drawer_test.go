c23446fb49ecce2b9535b44d9f12b3b21798ab0c
faiface <faiface@ksp.sk>
2017-07-03 00:22:45 +0200

    improve sprite.Draw(batch) benchmark

--- drawer_test.go
+++ drawer_test.go
@@ -1,14 +1,17 @@
 package pixel_test
 
 import (
+	"image"
 	"testing"
 
 	"github.com/faiface/pixel"
 )
 
 func BenchmarkSpriteDrawBatch(b *testing.B) {
-	sprite := pixel.NewSprite(nil, pixel.R(0, 0, 64, 64))
-	batch := pixel.NewBatch(&pixel.TrianglesData{}, nil)
+	img := image.NewRGBA(image.Rect(0, 0, 64, 64))
+	pic := pixel.PictureDataFromImage(img)
+	sprite := pixel.NewSprite(pic, pixel.R(0, 0, 64, 64))
+	batch := pixel.NewBatch(&pixel.TrianglesData{}, pic)
 	for i := 0; i < b.N; i++ {
 		sprite.Draw(batch, pixel.IM)
 	}
