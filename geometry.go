6215259c1deb8077fba22dd1053b6e0c976a6a64
faiface <faiface@ksp.sk>
2017-10-15 19:42:13 +0200

    add pixel.Clamp

--- geometry.go
+++ geometry.go
@@ -5,6 +5,20 @@ import (
 	"math"
 )
 
+// Clamp returns x clamped to the interval [min, max].
+//
+// If x is less than min, min is returned. If x is more than max, max is returned. Otherwise, x is
+// returned.
+func Clamp(x, min, max float64) float64 {
+	if x < min {
+		return min
+	}
+	if x > max {
+		return max
+	}
+	return x
+}
+
 // Vec is a 2D vector type with X and Y coordinates.
 //
 // Create vectors with the V constructor:
