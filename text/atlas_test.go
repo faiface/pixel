5ed20d54855317b35e8e26fcb43c81ffaa9ebf33
Peter Hellberg <peter@c7.se>
2018-01-19 23:09:48 +0100

    Change package to text_test

--- text/atlas_test.go
+++ text/atlas_test.go
@@ -1,18 +1,22 @@
-package text
+package text_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/faiface/pixel/text"
+)
 
 func TestAtlas7x13(t *testing.T) {
-	if Atlas7x13 == nil {
+	if text.Atlas7x13 == nil {
 		t.Fatalf("Atlas7x13 is nil")
 	}
 
 	for _, tt := range []struct {
 		runes []rune
 		want  bool
-	}{{ASCII, true}, {[]rune("ÅÄÖ"), false}} {
+	}{{text.ASCII, true}, {[]rune("ÅÄÖ"), false}} {
 		for _, r := range tt.runes {
-			if got := Atlas7x13.Contains(r); got != tt.want {
+			if got := text.Atlas7x13.Contains(r); got != tt.want {
 				t.Fatalf("Atlas7x13.Contains('%s') = %v, want %v", string(r), got, tt.want)
 			}
 		}
